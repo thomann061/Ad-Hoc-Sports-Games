35e2864c9f9da9815c359ff81f09c8f7adaa760f
thomann061 <thomann061@gmail.com>
2016-12-07 13:30:47 -0700

    Add User class

---
+++ v1/src/Model/User.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jthomann
+ * Date: 12/6/16
+ * Time: 7:34 PM
+ */
+
+namespace AdHocSportsGames\Model;
+
+/**
+ * Class User
+ * @package Model
+ */
+class User {
+    public $UserId;
+    public $UserName;
+    public $UserPassword;
+    public $UserFirstName;
+    public $UserLastName;
+}
