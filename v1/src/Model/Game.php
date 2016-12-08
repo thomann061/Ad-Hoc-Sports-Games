2f1b6edc469d8e2448ae7b98cca24142de86c38b
thomann061 <thomann061@gmail.com>
2016-12-08 09:39:02 -0700

    Add game model

---
+++ v1/src/Model/Game.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jthomann
+ * Date: 12/6/16
+ * Time: 7:34 PM
+ */
+
+namespace AdHocSportsGames\Model;
+
+/**
+ * Class Game
+ * @package Model
+ */
+class Game {
+    public $GameId;
+    public $GameName;
+    public $GameType;
+    public $GameLocation;
+    public $GameDateTime;
+}
