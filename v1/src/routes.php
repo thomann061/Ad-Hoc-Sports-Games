d10fb138213aea157aa9822e86ff1d201a591dc2
thomann061 <thomann061@gmail.com>
2016-12-08 09:39:49 -0700

    Add simplicity to routes

--- v1/src/routes.php
+++ v1/src/routes.php
@@ -5,12 +5,17 @@
  * Start User Routes
  */
 
-$app->get('/user', 'AdHocSportsGames\Controller\UserController:handleGetUsers');
-$app->get('/user/{id}', 'AdHocSportsGames\Controller\UserController:handleGetUser');
-$app->post('/user', 'AdHocSportsGames\Controller\UserController:handlePostUser');
-$app->delete('/user/{id}', 'AdHocSportsGames\Controller\UserController:handleDeleteUser');
-$app->put('/user/{id}', 'AdHocSportsGames\Controller\UserController:handlePutUser');
-$app->patch('/user/{id}', 'AdHocSportsGames\Controller\UserController:handlePatchUser');
+//put first name of controller here
+$controllers = array("Game", "User");
+
+foreach($controllers as $controller) {
+    $app->get('/' . strtolower($controller), "AdHocSportsGames\\Controller\\" . $controller . "Controller:handleGetAll");
+    $app->get('/' . strtolower($controller) . '/{id}', "AdHocSportsGames\\Controller\\" . $controller . "Controller:handleGet");
+    $app->post('/' . strtolower($controller), "AdHocSportsGames\\Controller\\" . $controller . "Controller:handlePost");
+    $app->delete('/' . strtolower($controller) . '/{id}', "AdHocSportsGames\\Controller\\" . $controller . "Controller:handleDelete");
+    $app->put('/' . strtolower($controller) . '/{id}', "AdHocSportsGames\\Controller\\" . $controller . "Controller:handlePut");
+    $app->patch('/' . strtolower($controller) . '/{id}', "AdHocSportsGames\\Controller\\" . $controller . "Controller:handlePatch");
+}
 
 /**
  * End User Routes
