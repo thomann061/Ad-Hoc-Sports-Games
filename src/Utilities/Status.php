8f8e1aadf03505ab20783d32b43d82f0d8fa0ad0
thomann061 <thomann061@gmail.com>
2016-12-13 11:17:08 -0700

    Add statuses for json

---
+++ src/Utilities/Status.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jthomann
+ * Date: 12/13/16
+ * Time: 9:57 AM
+ */
+
+namespace AdHocSportsGames\Utilities;
+
+
+class Status
+{
+    const SUCCESS = "Success";
+    const NO_RESULTS = "No results";
+    const ERROR = "Error";
+}
