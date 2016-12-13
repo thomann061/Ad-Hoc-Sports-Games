be616b5dc9ef7f342cbdbca3c08b2db1941eec8f
thomann061 <thomann061@gmail.com>
2016-12-13 11:16:23 -0700

    add rating model

---
+++ src/Model/Rating.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jthomann
+ * Date: 12/12/16
+ * Time: 9:39 AM
+ */
+
+namespace AdHocSportsGames\Model;
+
+
+class Rating {
+    public $RatingId;
+    public $RatingScore;
+    public $RatingDateTime;
+    public $GameId;
+}
